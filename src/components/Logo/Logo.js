f6284c8e89603441ce5a77a227b05faf8bf5fcc8
“AlexandruMucenic” <alexmucenic6@gmail.com>
2023-01-18 12:06:00 +0200

    updated page menu

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -5,9 +5,6 @@ function Logo() {
   return (
     <div className='logo-container'>
       <img src={logo} alt='logo' />
-      <h3>Alex</h3>
-      <h3>Mucenic</h3>
-      <span>Web developer</span>
     </div>
   );
 }
