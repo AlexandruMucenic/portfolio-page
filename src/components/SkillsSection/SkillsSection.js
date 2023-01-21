e0dc3fc58e19cf1c03034ead45d00250538ce1f5
“AlexandruMucenic” <alexmucenic6@gmail.com>
2023-01-21 19:14:04 +0200

    updated skills section

--- src/components/SkillsSection/SkillsSection.js
+++ src/components/SkillsSection/SkillsSection.js
@@ -1,26 +1,50 @@
 import ScrollDownLeft from '../ScrollDownLeft/ScrollDownLeft';
 import ScrollDownRight from '../ScrollDownRight/ScrollDownRight';
 
+import TagCloud from '@frank-mayer/react-tag-cloud';
+
 import './skillsSection.css';
 
 function SkillsSection() {
   return (
     <section className='skills-container' id='skills'>
-      <div className='description-container'>
+      <div className='skills-description-container'>
         <h2 className='skills-title'>My Skills</h2>
-        <p className='skills-description'>Something regarding my skills</p>
+        <p className='skills-description'>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum
+        </p>
       </div>
-      <div className='skills-canvas'>
-        <ul>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>JavaScript</li>
-          <li>React.js</li>
-          <li>PHP</li>
-          <li>Git</li>
-          <li>NPM</li>
-        </ul>
-      </div>
+
+      <TagCloud
+        options={w => ({
+          radius: Math.min(500, w.innerWidth, w.innerHeight) / 2,
+          maxSpeed: 'medium',
+        })}
+      >
+        {[
+          'HTML',
+          'CSS',
+          'JavaScript',
+          'React.js',
+          'PHP',
+          'git',
+          'CSS',
+          'JavaScript',
+          'React.js',
+          'PHP',
+          'git',
+          'HTML',
+        ]}
+      </TagCloud>
+
       <ScrollDownLeft />
       <ScrollDownRight />
     </section>
